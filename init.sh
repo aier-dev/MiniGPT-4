48a579e8644c6a27f0e932581a868d3fd513f604
xxai.art <xxai.art@gmail.com>
2023-05-04 03:14:49 +0800

    ♨

--- init.sh
+++ init.sh
@@ -4,16 +4,6 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
-if ! [ -x "$(command -v direnv)" ]; then
-  if ! [ -x "$(command -v apt-get)" ]; then
-    apt-get install -y direnv
-    echo -e "eval \"\$(direnv hook bash)\"" >>~/.bashrc
-    source ~/.bashrc
-  fi
-fi
-
-direnv allow
-
 pip install --upgrade pip
 ./sh/sharp.sh
 pip install -r requirements.txt
