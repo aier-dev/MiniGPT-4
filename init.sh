54f42b2e4fb9ba584a9ef8c5ce88f7ee0e0dbcde
xxai.art <xxai.art@gmail.com>
2023-05-04 02:54:55 +0800

    ♨

--- init.sh
+++ init.sh
@@ -4,6 +4,12 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
+if ! [ -x "$(command -v direnv)" ]; then
+  if ! [ -x "$(command -v apt-get)" ]; then
+    apt-get install -y direnv
+  fi
+fi
+
 direnv allow
 
 pip install --upgrade pip
