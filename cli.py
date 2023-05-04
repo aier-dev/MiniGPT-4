f3011a024aba67fbf39158482a6c94ee4e14c833
xxai.art <xxai.art@gmail.com>
2023-05-05 07:59:14 +0800

    ♨

--- cli.py
+++ cli.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     for i in range(10):
         imgli.append('/Users/z/art/MiniGPT-4/img/%s.jpg' % i)
     qli = [
-        'Tag this image, the tags are separated by commas',
+        'Generate label for this image, the label use one or two words',
         'Generate a very detailed description for this image, description start with "This image shows "',
         'Describe this image in a simple sentence, description start with "The image depicts "',
     ]
