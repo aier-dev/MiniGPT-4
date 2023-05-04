08f6bc520d55d20a477b9312febca510d5931f5c
xxai.art <xxai.art@gmail.com>
2023-05-05 06:48:02 +0800

    ♨

--- init.sh
+++ init.sh
@@ -5,6 +5,12 @@ cd $DIR
 set -ex
 
 pip install --upgrade pip
-./sh/sharp.sh
 pip install -r requirements.txt
+
+if [[ $(uname) != *"Darwin"* ]]; then
+  pip install eva-decord
+else
+  pip install decord
+fi
+#./sh/sharp.sh
 ./down.sh
